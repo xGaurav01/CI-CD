56ede15e4e4b6e99ffaa3ec0dc056a0615147b0c
xGaurav01 <gauravsharma76500@gmail.com>
2024-03-31 18:44:05 +0530

    first

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Run Git Hub Actions
         </a>
       </header>
     </div>
